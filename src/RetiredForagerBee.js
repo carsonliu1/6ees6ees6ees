7173587ce9c8c34afebac64cc8bf21685d03cdd4
Carson <liucarson@yahoo.com>
2022-06-12 12:03:40 -0700

    updates

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, job, canFly, forage, color) {
+    super(age, job, canFly, forage, color)
+    this.age = 40
+    this.job = 'gamble'
+    this.canFly = false
+    this.color = 'grey'
+  }
+  forage() {
+    return 'I am too old, let me play cards instead'
+  }
+  gamble() {
+    super.forage()
+  }
 };
